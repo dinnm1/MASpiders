fe7396e678637d095f7bfeb6d9571eca78f473eb
drozado <drozado@gmail.com>
2018-08-29 16:19:11 +1200

    eliminated hardcoded year in URL

--- nyt/nyt/spiders/nyt.py
+++ nyt/nyt/spiders/nyt.py
@@ -10,12 +10,32 @@ from nyt.items import NewsItem
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 
 class TestSpider(CrawlSpider):
+
+    def __init__(self, uni=None, *args, **kwargs):
+        self.year = str(year)
+        super(TestSpider, self).__init__(*args, **kwargs)
+        self.start_urls = ['http://spiderbites.nytimes.com/' + year + '/']  # The starting page for the crawls
+
     name = "nyt" #This name will help while running the crawler itself
     allowed_domains = ["nytimes.com"] #which domains are accessible for this crawler
+
     start_urls = ['http://spiderbites.nytimes.com/1997/'] #initial URLs that are to be accessed first
 
+    custom_settings = {
+        # 'LOG_FILE': r"I:\COURSES\EAD\AITEIT3\BITY3\IN700001 Project\NLP\david\bu.log",
+        'LOG_FILE': "./data/gen.log",
+        'LOG_ENABLED': True,
+        'LOG_STDOUT': True,
+        'LOG_LEVEL': 'INFO',  # DEBUG, INFO
+        'DEPTH_LIMIT': 3,
+        # 'FEED_URI': r'file:///I:/COURSES/EAD/AITEIT3/BITY3/IN700001 Project/NLP/david/%(name)sDepthLimit%(depth_limit)s.csv',
+        # 'FEED_URI': "file:///C:/Users/drozado/data/gen.csv",
+        'FEED_URI': r"./data/%(self.year)s.csv",
+        'FEED_FORMAT': 'csv',
+        'AUTOTHROTTLE_ENABLED': False,
+    }
     rules = (Rule(LxmlLinkExtractor(
-            allow=("1997")),
+            allow=(self.year)),
             follow=True,
             callback='parse_item'),)
 		
